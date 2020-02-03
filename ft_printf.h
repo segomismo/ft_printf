c7c129afed5640f0c6f9b8775498857d8203f209
segomismo <60438612+segomismo@users.noreply.github.com>
2020-02-03 17:38:06 +0100

    Empieza a tener forma2
    En el futuro, sustituir el fr_printf2 . por el 1 para que el 1 actual no sea relevante

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rufranci <rufranci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 11:58:01 by rufranci          #+#    #+#             */
-/*   Updated: 2020/01/31 12:54:28 by marvin           ###   ########.fr       */
+/*   Updated: 2020/02/03 17:35:42 by rufranci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,8 @@
 
 typedef	struct t_printf {
 	va_list arg;
-	int		c;
-	char	*ce;
-	int		size;
-	int		cont;
+	int		size;//cuenta la cantidad de letras que ha impreso, el size total se puede hacer como cont+size.sieze solo lo cuento cuando imprimo mas de un caracter en los argumentos
+	int		cont;//sirve para la posicion en la que esta
 }t_printf;
 #endif
 
